cc484bfbefb0db33fc1deda5e6a7a882e5784056
Martin Gil <martin.gil.o@gmail.com>
2024-11-26 12:35:39 -0300

    Updated requirements

--- utils.py
+++ utils.py
@@ -5,9 +5,9 @@ import subprocess
 load_dotenv()
 
 credenciales = {
-    "type": "service_account",
-    "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
-    "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
+    "type": os.environ.get('type'),
+    "project_id": os.environ.get('project_id'),
+    "private_key_id": os.environ.get('private_key_id'),
     "private_key": os.environ.get('private_key').replace('\\n','\n'),
     "client_email": os.environ.get('client_email'),
     "client_id": os.environ.get('client_id'),
